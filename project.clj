c466a8cd8f5044b012bf3d81b225713f2a86f1b2
Justin Kramer <jkkramer@gmail.com>
2012-08-24 21:40:35 -0400

    0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject honeysql "0.1.0"
+(defproject honeysql "0.1.1-SNAPSHOT"
   :description "SQL as Clojure data structures"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
