f9f20871635917e8d6ff6203f7b2ec47d07f2fab
Justin Kramer <jkkramer@gmail.com>
2013-08-06 13:55:49 -0400

    bump clojure version

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "SQL as Clojure data structures"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]])
