78ac2e65fdf56cfc729aa290a79af4a2b0508a36
Mike Blume <michael.blume@climate.com>
2015-01-23 12:50:08 -0800

    upgrade clojure dependency

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "SQL as Clojure data structures"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]])
