ff656ebf764f2696390a4b80609595d266c2dbc4
Mike Blume <michael.blume@climate.com>
2015-01-23 12:50:08 -0800

    upgrade clojure dependency

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :url http://www.github.com/MichaelBlume/honeysql
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]])
