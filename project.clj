1bec3a6143b8bae3bec47f0e7b025da9822924a2
Mike Blume <michael.blume@climate.com>
2015-03-09 12:08:29 -0700

    add :url to project.clj

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "SQL as Clojure data structures"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url https://github.com/jkk/honeysql
   :dependencies [[org.clojure/clojure "1.6.0"]])
