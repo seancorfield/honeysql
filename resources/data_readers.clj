83f16780b082457710472602e7ff66193fabd7c0
Sean Corfield <sean@corfield.org>
2018-06-25 12:30:15 -0700

    Add #sql/inline data reader

--- resources/data_readers.clj
+++ resources/data_readers.clj
@@ -1,4 +1,5 @@
 {sql/call honeysql.types/read-sql-call
+ sql/inline honeysql.types/read-sql-inline
  sql/raw honeysql.types/read-sql-raw
  sql/param honeysql.types/read-sql-param
  sql/array honeysql.types/read-sql-array
