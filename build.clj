ae29147a2229193e13a4d0871c61be8e6ee6a41e
lread <lee@dlread.com>
2021-08-30 21:08:28 -0400

    PR review feedback: move gen-doc-tests build code
    Move the code that generates doc tests out from build.clj
    to its own source runnable via main.
    
    This isolates gen-doc-tests work to its own process
    during the build.
    
    I arbitrarily:
    - chose 'build' as the folder for build helper sources
    - namespaced our first build helper under 'honey.gen-doc-tests'

--- build.clj
+++ build.clj
@@ -10,10 +10,8 @@
 
   clojure -A:deps -T:build help/doc"
 
-  (:require [babashka.fs :as fs]
-            [clojure.tools.build.api :as b]
-            [org.corfield.build :as bb]
-            [lread.test-doc-blocks :as tdb]))
+  (:require [clojure.tools.build.api :as b]
+            [org.corfield.build :as bb]))
 
 (def lib 'com.github.seancorfield/honeysql)
 (def version (format "2.0.%s" (b/git-count-revs nil)))
@@ -21,33 +19,8 @@
 (defn eastwood "Run Eastwood." [opts]
   (-> opts (bb/run-task [:eastwood])))
 
-(defn gen-doc-tests "Generate tests from doc code blocks" [opts]
-  (let [target "target/test-doc-blocks"
-        success-marker (fs/file target "SUCCESS")
-        docs ["README.md"
-              "doc/clause-reference.md"
-              "doc/differences-from-1-x.md"
-              "doc/extending-honeysql.md"
-              "doc/general-reference.md"
-              "doc/getting-started.md"
-              "doc/postgresql.md"
-              "doc/special-syntax.md"]
-        regen-reason (if (not (fs/exists? success-marker))
-                       "a previous successful gen result not found"
-                       (let [newer-thans (fs/modified-since target
-                                                            (concat docs
-                                                                    ["build.clj" "deps.edn"]
-                                                                    (fs/glob "src" "**/*.*")))]
-                         (when (seq newer-thans)
-                           (str "found files newer than last gen: " (mapv str newer-thans)))))]
-    (if regen-reason
-      (do
-        (fs/delete-if-exists success-marker)
-        (println "gen-doc-tests: Regenerating:" regen-reason)
-        (tdb/gen-tests {:docs docs})
-        (spit success-marker "SUCCESS"))
-      (println "gen-doc-tests: Tests already successfully generated")))
-  opts)
+(defn gen-doc-tests "Generate tests from doc code blocks." [opts]
+  (-> opts (bb/run-task [:gen-doc-tests])))
 
 (defn run-doc-tests
   "Generate and run doc tests.
