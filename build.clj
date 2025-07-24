89ff5fb389f14a1ac219761f3d99c1bc100c0c4a
Sean Corfield <sean@corfield.org>
2025-07-23 22:21:11 -0400

    complete bbification
    Signed-off-by: Sean Corfield <sean@corfield.org>

--- build.clj
+++ build.clj
@@ -1,13 +1,11 @@
 (ns build
   "HoneySQL's build script.
 
-  clojure -T:build ci
-
-  clojure -T:build run-doc-tests :aliases '[:cljs]'
+  clojure -T:build jar
+  clojure -T:build deploy
 
   Run tests:
-  clojure -X:test
-  clojure -X:test:1.12
+  bb test
 
   For more information, run:
 
@@ -23,47 +21,6 @@
 (def version (the-version (b/git-count-revs nil)))
 (def snapshot (the-version "9999-SNAPSHOT"))
 (def class-dir "target/classes")
-
-(defn- run-task [aliases]
-  (println "\nRunning task for" (str/join "," (map name aliases)))
-  (let [basis    (b/create-basis {:aliases aliases})
-        combined (t/combine-aliases basis aliases)
-        cmds     (b/java-command
-                  {:basis      basis
-                   :main      'clojure.main
-                   :main-args (:main-opts combined)})
-        {:keys [exit]} (b/process cmds)]
-    (when-not (zero? exit) (throw (ex-info "Task failed" {})))))
-
-(defn eastwood "Run Eastwood." [opts]
-  (run-task [:eastwood])
-  opts)
-
-(defn gen-doc-tests "Generate tests from doc code blocks." [opts]
-  (run-task [:gen-doc-tests])
-  opts)
-
-(defn run-doc-tests
-  "Generate and run doc tests.
-
-  Optionally specify :aliases vector:
-  [:1.10] -- test against Clojure 1.10.3 (the default)
-  [:1.11] -- test against Clojure 1.11.4
-  [:1.12] -- test against Clojure 1.12.1
-  [:cljs] -- test against ClojureScript"
-  [{:keys [aliases] :as opts}]
-  (gen-doc-tests opts)
-  (run-task (-> [:test :runner :test-doc]
-                (into aliases)
-                (into (if (some #{:cljs} aliases)
-                        [:test-doc-cljs]
-                        [:test-doc-clj]))))
-  opts)
-
-(defn test "Run basic tests." [opts]
-  (run-task [:test :runner :1.11])
-  (run-task [:test :runner :cljs])
-  opts)
 
 (defn- pom-template [version]
   [[:description "SQL as Clojure data structures."]
@@ -94,29 +51,6 @@
            :target    "target"
            :src-dirs  ["src"]
            :pom-data  (pom-template version))))
-
-(defn ci
-  "Run the CI pipeline of tests (and build the JAR).
-
-  Default Clojure version is 1.10.3 (:1.10) so :elide
-  tests for #409 on that version."
-  [opts]
-  (let [aliases [:cljs :elide :1.11 :1.12]
-        opts    (jar-opts opts)]
-    (b/delete {:path "target"})
-    (doseq [alias aliases]
-      (run-doc-tests {:aliases [alias]}))
-    (eastwood opts)
-    (doseq [alias aliases]
-      (run-task [:test :runner alias]))
-    (b/delete {:path "target"})
-    (println "\nWriting pom.xml...")
-    (b/write-pom opts)
-    (println "\nCopying source...")
-    (b/copy-dir {:src-dirs ["src"] :target-dir class-dir})
-    (println "\nBuilding" (:jar-file opts) "...")
-    (b/jar opts))
-  opts)
 
 (defn jar "Build the JAR." [opts]
   (let [opts (jar-opts opts)]
